2cea127ec7c412debc7287eee33063ab7c96bcd3
lwj0831 <wjl0831@gmail.com>
2025-04-29 11:33:18 +0900

    feat: Implement Level_1 works

---
+++ src/main/java/org/kakaoTechCampus/calculatorProject/CaculatorApplication.java
@@ -0,0 +1,60 @@
+package org.kakaoTechCampus.calculatorProject;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CaculatorApplication {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            int num1;
+            int num2;
+            String operator;
+
+            try {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                num1 = scanner.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                num2 = scanner.nextInt();
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                operator = scanner.next();
+            }catch(InputMismatchException e){
+                System.out.println("InputMismatchException occurs!");
+                break;
+            }
+
+            int result=0;
+            switch (operator) {
+                case "+":
+                    result = num1+num2;
+                    System.out.println("결과: " + num1 + " + " + num2 + " = " + result);
+                    break;
+                case "-":
+                    result = num1-num2;
+                    System.out.println("결과: " + num1 + " - " + num2 + " = " + result);
+                    break;
+                case "*":
+                    result = num1*num2;
+                    System.out.println("결과: " + num1 + " * " + num2 + " = " + result);
+                    break;
+                case "/":
+                    try {
+                        result = num1 / num2;
+                        System.out.println("결과: " + num1 + " / " + num2 + " = " + result);
+                        break;
+                    }catch (ArithmeticException e){
+                        System.out.println("divide by zero error occurs!");
+                        break;
+                    }
+                default:
+                    System.out.println("operator error");
+            }
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String exitResponse = scanner.next();
+            if(exitResponse.equals("exit")) break;
+        }
+    }
+}
